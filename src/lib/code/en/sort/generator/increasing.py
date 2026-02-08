7e7bf97a4d6bf2d42eecc20442bfc7e000d13e5c
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2026-02-08 20:54:57 +0100

    add

--- src/lib/code/en/sort/generator/increasing.py
+++ src/lib/code/en/sort/generator/increasing.py
@@ -1,5 +1,5 @@
 def general(n):
   return list(range(1, n + 1))
 
-sizes = [10, 100, 1000, 10000, 1000000]
-INPUTS = ["\n".join(map(str, general(n))) + "\n" for n in sizes]
+sizes = [10, 100, 200]
+INPUTS = [" ".join(map(str, general(n))) + "\n" for n in sizes]
