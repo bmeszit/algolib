d5bbfad40d7ac62f30c893c200cffd51d3f7e45f
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2026-02-08 22:19:47 +0100

    add

--- src/lib/code/hu/search/algo/binker.py
+++ src/lib/code/hu/search/algo/binker.py
@@ -1,15 +1,15 @@
 def binker(T, eleje, vege, x):
   if eleje <= vege:
-    kozepe = (eleje+vege) // 2
+    kozepe = (eleje + vege) // 2
     if T[kozepe] == x:
-      return kozepe+1
-    elif T[kozepe] < x:
+      return kozepe + 1
+    elif x < T[kozepe]:
       return binker(T, eleje, kozepe-1, x)
-    else: # T[kozepe] > x
+    else:  # T[kozepe] < x
       return binker(T, kozepe+1, vege, x)
   else:
     return -1
 
 T = list(map(int, input().split()))
 x = int(input())
-print(binker(T, 0, len(T)-1, x))
+print(binker(T, 0, len(T) - 1, x))
