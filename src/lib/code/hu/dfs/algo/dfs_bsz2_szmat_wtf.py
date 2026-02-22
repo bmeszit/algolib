df422d271f47c1553111571502a335791948f8b6
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2026-02-22 22:01:57 +0100

    add

---
+++ src/lib/code/hu/dfs/algo/dfs_bsz2_szmat_wtf.py
@@ -0,0 +1,50 @@
+from copy import deepcopy
+
+def dfs(G, s):
+  n = len(G)
+  hol = [0]*n
+  globhol = 0
+  mszam = [None]*n; mszam[s] = 1
+  bszam = [None]*n
+  elozo = [None]*n
+  MSZAM = 1; BSZAM = 0; aktiv_csucs = s
+  while True:
+    while hol[aktiv_csucs] < n:
+      v = hol[aktiv_csucs]
+      hol[aktiv_csucs]+=1
+      if G[aktiv_csucs][v] and mszam[v] is None:
+        MSZAM += 1; mszam[v] = MSZAM
+        elozo[v] = aktiv_csucs
+        aktiv_csucs = v
+        break
+    else:
+      BSZAM += 1; bszam[aktiv_csucs] = BSZAM
+      if elozo[aktiv_csucs] != None:
+        aktiv_csucs = elozo[aktiv_csucs]
+      else:
+        while globhol < n:
+          v = globhol
+          globhol+=1
+          if mszam[v] == None:
+            aktiv_csucs = v
+            MSZAM += 1; mszam[aktiv_csucs] = MSZAM
+            break
+            
+        else:
+          break
+  return elozo, mszam, bszam
+
+n, m = map(int, input().split())
+G = [[False] * n for i in range(n)]
+for i in range(m):
+  u, v = map(int, input().split())
+  G[u][v] = True
+s = int(input())
+
+MERES_KEZD()
+elozo, mszam, bszam = dfs(deepcopy(G), s)
+MERES_VEG()
+
+print("Szülő csúcsok:"); print(elozo); print()
+print("Mélységi számok:"); print(mszam); print()
+print("Befejezési számok:"); print(bszam); print()
