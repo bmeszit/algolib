6a77cd40f5cc25dfb7dc94d2f1701ec97aed1d06
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2026-09-08 18:44:00 +0200

    track SPA paths with hostnames

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,29 @@
 <script>
+  import { afterNavigate } from "$app/navigation";
   import "../lib/i18n";
   import { page } from "$app/state";
   import favicon from "$lib/assets/favicon.svg";
   import Header from "$lib/Header.svelte";
   import Footer from "$lib/Footer.svelte";
   import "../app.scss";
+
+  let lastTrackedPath = "";
+
+  afterNavigate(() => {
+    const path = `${window.location.pathname}${window.location.search}${window.location.hash}`;
+    if (path === lastTrackedPath) return;
+    lastTrackedPath = path;
+
+    let attempts = 0;
+    const track = () => {
+      if (typeof window.goatcounter?.count === "function") {
+        window.goatcounter.count({ path: window.location.host + path });
+      } else if (attempts++ < 100) {
+        setTimeout(track, 50);
+      }
+    };
+    track();
+  });
 
   import { createCodeRepo } from "$lib";
   import { defaultCode } from "$lib";
